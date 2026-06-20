2a4cb1f4e0aaa99e9fcea4ecc39e80b77e7710cb
listenzcc <listenzcc@mail.bnu.edu.cn>
2026-06-20 09:00:59 +0800

    Update read_eeg.py to read EEG data from .mat files and integrate MNE for processing; adjust data directory and logging; modify read_mat.py to visualize MSClass data.

--- read_eeg.py
+++ read_eeg.py
@@ -19,10 +19,23 @@ Functions:
 # %% ---- 2026-06-17 ------------------------
 # Requirements and constants
 import mne
+import mat73
 from util.easy_imports import *
 
 # %%
-DATA_DIR = Path('./data/set')
+DATA_DIR = Path('./data/eeg-mat')
+
+# %%
+ch_names = [e.split('\t')[1]
+            for e in open(DATA_DIR / 'chan_labels.txt').read().split('\n') if '\t' in e]
+print(f'{ch_names=}')
+
+times = [float(e) / 1000 for e in open(DATA_DIR /
+                                       'times_ms.txt').read().split()]
+print(f'{times=}')
+
+sfreq = 1000  # Hz
+
 
 # %% ---- 2026-06-17 ------------------------
 # Function and class
@@ -30,20 +43,31 @@ DATA_DIR = Path('./data/set')
 
 # %% ---- 2026-06-17 ------------------------
 # Play ground
-set_files = sorted(DATA_DIR.rglob('*.set'))
+set_files = sorted(DATA_DIR.rglob('*.mat'))
 logger.info(f'Found .set files: {len(set_files)=}')
 
 src = set_files[0]
-src = Path('data/set/T100/post/10.set')
 
 logger.info(f'Read {src=}')
+mat = mat73.loadmat(src)
 
-epochs = mne.io.read_epochs_eeglab(src)
+# raw_data shape is (n_channels, n_times, n_epochs)
+raw_data = mat['data']
+# data shape convert into (n_epochs, n_channels, n_times)
+data = raw_data.transpose([2, 0, 1])
+
+info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types='eeg')
+events = np.array([[i, 0, 1] for i in range(len(data))])  # 全部标为事件1
+epochs = mne.EpochsArray(data, info, events=events, tmin=times[0])
+# ===== 关键代码：设置标准 10-20 系统蒙太奇 =====
+montage = mne.channels.make_standard_montage('standard_1020')
+epochs.set_montage(montage)
 print(epochs)
-
 
 # %% ---- 2026-06-17 ------------------------
 # Pending
+evoked = epochs.average()
+evoked.plot_joint()
 
 
 # %% ---- 2026-06-17 ------------------------
